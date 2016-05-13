e16721e0aaa44e14d16365ba1e2e22d41c7e16f7
Basma <samaha.basma@gmail.com>
2016-05-13 09:50:29 -0700

    checkppoint.29 completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,33 +1,24 @@
+var animatePoints = function() {
+  var revealPoint = function() {
+    $(this).addClass('point-animate-in-finish');
+  }
 
-
-var pointsArray = document.getElementsByClassName('point');
-
-var revealPoint = function(point){
-  point.style.opacity = 1;
-  point.style.transform = "scaleX(1) translateY(0)";
-  point.style.msTransform = "scaleX(1) translateY(0)";
-  point.style.WebkitTransform = "scaleX(1) translateY(0)";
-}
-
-var animatePoints = function(points) {
-    forEach( points, revealPoint );
+  $.each($('.point'), revealPoint);
 };
 
-window.onload = function(){
-
-  if(window.innerHeight > 950){
+$(window).load(function() {
+  // Automatically animate the points on a tall screen where scrolling can't trigger the animation
+  if ($(window).height() > 950) {
     animatePoints(pointsArray);
   }
 
-  var sellingPoints = document.getElementsByClassName('selling-points')[0];
-  var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+  var scrollDistance = $('.selling-points').offset().top - $(window).height() + 200;
 
-  window.addEventListener('scroll', function(event) {
-
-    if (document.body.scrollTop >= scrollDistance) {
-      animatePoints(pointsArray);
+  $(window).scroll( function(points) {
+    if ( $(window).scrollTop() >= scrollDistance) {
+      animatePoints();
     }
-
   });
-
-};
+});
+Status API Training Shop Blog About
+© 2016 GitHub, Inc. Terms Privacy Security Contact Help
